3b83fbd3a5437558530be477518e19343ef71f65
Hanji <minh-hanh.nguyen.auditeur@lecnam.net>
2023-07-17 14:56:35 +0200

    layout first upload

---
+++ flutter_layout_article-master/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout_article/examples.dart';
+import 'package:flutter_layout_article/layout_article.dart';
+
+void main() => runApp(HomePage());
+
+//////////////////////////////////////////////////
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => FlutterLayoutArticle(<Example>[
+        Example1(),
+        Example2(),
+        Example3(),
+        Example4(),
+        Example5(),
+        Example6(),
+        Example7(),
+        Example8(),
+        Example9(),
+        Example10(),
+        Example11(),
+        Example12(),
+        Example13(),
+        Example14(),
+        Example15(),
+        Example16(),
+        Example17(),
+        Example18(),
+        Example19(),
+        Example20(),
+        Example21(),
+        Example22(),
+        Example23(),
+        Example24(),
+        Example25(),
+        Example26(),
+        Example27(),
+        Example28(),
+        Example29(),
+      ]);
+}
