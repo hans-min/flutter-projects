422180d7fd188fe43aaf6308e80602af269d00e0
Hanji <kaitothedraggy@gmail.com>
2023-10-07 20:31:15 +0200

    remove api key

--- myOty-Flutter-main/ios/Runner/AppDelegate.swift
+++ myOty-Flutter-main/ios/Runner/AppDelegate.swift
@@ -8,8 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    // TODO: Add your Google Maps API key
-    //GMSServices.provideAPIKey("AIzaSyDer0DTGERzn3MBVvECvRvJNZT6Hx5b654")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
